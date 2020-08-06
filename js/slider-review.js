40999753d56c74b229144c13d3d34f97180eaa55
Demchenko Vyacheslav <frontend-ninja@yandex.ru>
2020-08-06 10:25:58 +0300

    Made both sliders

--- js/slider-review.js
+++ js/slider-review.js
@@ -1,20 +1,61 @@
 $(document).ready(function () {
 
-    let owl = $('.reviews-slider.owl-carousel');
+    if ($(window).width() >= 1365) {
 
-    owl.owlCarousel({
-        items: 1,
-        autoWidth: true,
-        nav: false,
-        center: true,
-        responsive: {
-            0: {
-                margin: 5
-            },
-            1365: {
-                margin: 0
-            },
+        $('.reviews-slider-desktop-inner').owlCarousel({
+            items: 1,
+            nav: true,
+            autoWidth: true,
+            center: true,
+            loop: true,
+            lazyLoad: true
+        });
+    } else if ($(window).width() < 1365) {
+        $('.reviews-slider-desktop-inner').trigger('destroy.owl.carousel');
+    }
+
+    if ($(window).width() < 1365) {
+
+        $('.reviews-slider.owl-carousel').owlCarousel({
+            items: 1,
+            autoWidth: true,
+            nav: false,
+            center: true,
+            loop: true,
+            stagePadding: 20,
+            margin: 10
+        });
+    } else if ($(window).width() >= 1365) {
+        $('.reviews-slider.owl-carousel').trigger('destroy.owl.carousel');
+    }
+
+    $(window).resize(function () {
+
+        if ($(window).width() < 1365) {
+            $('.reviews-slider-desktop-inner').trigger('destroy.owl.carousel');
+            $('.reviews-slider.owl-carousel').owlCarousel({
+                items: 1,
+                autoWidth: true,
+                nav: false,
+                center: true,
+                loop: true,
+                stagePadding: 20,
+                margin: 10
+            });
         }
+
+        if ($(window).width() >= 1365) {
+            $('.reviews-slider.owl-carousel').trigger('destroy.owl.carousel');
+            $('.reviews-slider-desktop-inner').owlCarousel({
+                items: 1,
+                nav: true,
+                autoWidth: true,
+                center: true,
+                loop: true,
+                lazyLoad: true
+            });
+        }
+
     });
 
 });
