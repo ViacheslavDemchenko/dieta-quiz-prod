1ae64d76101bd2d9be650283f7efeeb665704771
Demchenko Vyacheslav <frontend-ninja@yandex.ru>
2020-08-13 12:03:50 +0300

    Payment href corrected

--- js/result-show.js
+++ js/result-show.js
@@ -132,7 +132,7 @@ document.addEventListener('DOMContentLoaded', function () {
           modalMax.classList.add('modal-max--active');
           overlay.classList.add('overlay--active');
         } else {
-          window.location.href = 'personal-profile.html';
+          window.location.href = 'payment.html';
         }
       });
     });
